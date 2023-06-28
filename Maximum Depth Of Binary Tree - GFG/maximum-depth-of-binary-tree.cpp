3ceabde13905d483abfe3167bb972aae64441e16
White8654 <108630063+White8654@users.noreply.github.com>
2023-06-28 11:50:08 +0530

    Added solution - LeetHub

--- Maximum Depth Of Binary Tree - GFG/maximum-depth-of-binary-tree.cpp
+++ Maximum Depth Of Binary Tree - GFG/maximum-depth-of-binary-tree.cpp
@@ -93,12 +93,7 @@ class Solution{
   public:
     /*You are required to complete this method*/
     int maxDepth(Node *root) {
-       if(root == NULL)
-       return 0;
-       int l = maxDepth(root->left);
-       int r = maxDepth(root->right);
-       
-       return 1 + max(l,r);
+      return !root ? 0 : 1 + max(maxDepth(root->left),maxDepth(root->right));
     }
 };
 
